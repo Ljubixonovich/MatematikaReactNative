e9e1cf1b5f7a07362d2dd3d11f7bd3f1f51e1640
Ljubixonovich <vikvikvuk@gmail.com>
2019-02-21 15:08:36 +0100

    exctracted ballon component, to do reset game

--- src/components/Ballon/redBallon.js
+++ src/components/Ballon/redBallon.js
@@ -1,16 +1,53 @@
 import React, { Component } from 'react';
 import { Image, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 
+import redBallonImage from '../../media/images/redbubble.png';
+import zvezdiceImage from '../../media/images/zvezdice.png';
+import transparentImage from '../../media/images/transparent.png';
+
 
 class redBallon extends Component {
    constructor(props) {
       super(props);
    }
 
+   state = {
+      ballonPressed: false,
+      source: redBallonImage
+   };
+
+   reset = () => {
+      this.setState({
+         ballonPressed: false,
+         source: redBallonImage
+      });
+   }
+
+   ballonPressed = () => {
+      if (this.state.source === redBallonImage){
+         this.setState(prevState => {
+            return {
+               ...prevState,
+               ballonPressed: true, 
+               source: zvezdiceImage                       
+            }
+         }); 
+         setTimeout(() => {
+            this.setState(prevState => {
+               return {
+                  ...prevState,
+                  source: transparentImage              
+               }
+            });
+         }, 100);
+         this.props.onPress();       
+      }      
+   }
+
    render(){
       return (
-         <TouchableWithoutFeedback onPress={this.props.onPress}>
-            <Image source={this.props.source} style={styles.image} />
+         <TouchableWithoutFeedback onPress={this.ballonPressed}>
+            <Image source={this.state.source} style={styles.image} />
          </TouchableWithoutFeedback>
       );
    }
--- src/screens/grade1/razlika/zadatak/zadatak1.js
+++ src/screens/grade1/razlika/zadatak/zadatak1.js
@@ -20,43 +20,8 @@ class RazlikaZadatak1Screen1 extends Component {
       this.setState({
          viewMode: Dimensions.get('window').height > 500 ? 'portrait' : 'landscape',
          ballonClickedCount: 0,
-         ballon1: {
-            source: redBallonImage
-         },
-         ballon2: {
-            source: redBallonImage
-         },
-         ballon3: {
-            source: redBallonImage
-         },
-         ballon4: {
-            source: redBallonImage
-         },
-         ballon5: {
-            source: redBallonImage
-         },
-         ballon6: {
-            source: redBallonImage
-         },
-         ballon7: {
-            source: redBallonImage
-         },
-         ballon8: {
-            source: redBallonImage
-         },
-         ballon9: {
-            source: redBallonImage
-         },
-         ballon10: {
-            source: redBallonImage
-         },
-         ballon11: {
-            source: redBallonImage
-         },
-         ballon12: {
-            source: redBallonImage
-         }
       });
+      
    };
 
    componentWillMount() {
@@ -70,9 +35,7 @@ class RazlikaZadatak1Screen1 extends Component {
    updateStyles = dims => {
       this.setState(prevState => {
          return {
-            ballons: {
-               ...prevState.ballons                            
-            },
+            ...prevState,
             viewMode: dims.window.height > 500 ? 'portrait' : 'landscape'
          }
       });
@@ -83,305 +46,15 @@ class RazlikaZadatak1Screen1 extends Component {
    }
 
    imageClick = (id) => { 
-      switch (id) {
-         case 1:
-            if (this.state.ballon1.source === redBallonImage){            
-               this.setState(prevState => {
-                  return {
-                     ...prevState,
-                     ballon1: {
-                        source: zvezdiceImage
-                     },
-                     ballonClickedCount: prevState.ballonClickedCount + 1         
-                  }
-               });
-               setTimeout(() => {
-                  this.setState(prevState => {
-                     return {
-                        ...prevState,
-                        ballon1: {
-                           source: transparentImage
-                        }               
-                     }
-                  });
-               }, 200);
-            }
-            break;
-         
-         case 2:
-            if (this.state.ballon2.source === redBallonImage){
-               this.setState(prevState => {
-                  return {
-                     ...prevState,
-                     ballon2: {
-                        source: zvezdiceImage
-                     },
-                     ballonClickedCount: prevState.ballonClickedCount + 1            
-                  }
-               });
-               setTimeout(() => {
-                  this.setState(prevState => {
-                     return {
-                        ...prevState,
-                        ballon2: {
-                           source: transparentImage
-                        }               
-                     }
-                  });
-               }, 200);
-            }
-            break;
-
-         case 3:
-            if (this.state.ballon3.source === redBallonImage){
-               this.setState(prevState => {
-                  return {
-                     ...prevState,
-                     ballon3: {
-                        source: zvezdiceImage
-                     },
-                     ballonClickedCount: prevState.ballonClickedCount + 1            
-                  }
-               });
-               setTimeout(() => {
-                  this.setState(prevState => {
-                     return {
-                        ...prevState,
-                        ballon3: {
-                           source: transparentImage
-                        }               
-                     }
-                  });
-               }, 200);
-            }
-            break;
-
-         case 4:
-            if (this.state.ballon4.source === redBallonImage){
-               this.setState(prevState => {
-                  return {
-                     ...prevState,
-                     ballon4: {
-                        source: zvezdiceImage
-                     },
-                     ballonClickedCount: prevState.ballonClickedCount + 1            
-                  }
-               });
-               setTimeout(() => {
-                  this.setState(prevState => {
-                     return {
-                        ...prevState,
-                        ballon4: {
-                           source: transparentImage
-                        }               
-                     }
-                  });
-               }, 200);
-            }
-            break;
-
-         case 5:
-            if (this.state.ballon5.source === redBallonImage){
-               this.setState(prevState => {
-                  return {
-                     ...prevState,
-                     ballon5: {
-                        source: zvezdiceImage
-                     },
-                     ballonClickedCount: prevState.ballonClickedCount + 1            
-                  }
-               });
-               setTimeout(() => {
-                  this.setState(prevState => {
-                     return {
-                        ...prevState,
-                        ballon5: {
-                           source: transparentImage
-                        }               
-                     }
-                  });
-               }, 200);
-            }
-            break;
-
-         case 6:
-            if (this.state.ballon6.source === redBallonImage){
-               this.setState(prevState => {
-                  return {
-                     ...prevState,
-                     ballon6: {
-                        source: zvezdiceImage
-                     },
-                     ballonClickedCount: prevState.ballonClickedCount + 1            
-                  }
-               });
-               setTimeout(() => {
-                  this.setState(prevState => {
-                     return {
-                        ...prevState,
-                        ballon6: {
-                           source: transparentImage
-                        }               
-                     }
-                  });
-               }, 200);
-            }
-         break;
-
-         case 7:
-            if (this.state.ballon7.source === redBallonImage){
-               this.setState(prevState => {
-                  return {
-                     ...prevState,
-                     ballon7: {
-                        source: zvezdiceImage
-                     },
-                     ballonClickedCount: prevState.ballonClickedCount + 1            
-                  }
-               });
-               setTimeout(() => {
-                  this.setState(prevState => {
-                     return {
-                        ...prevState,
-                        ballon7: {
-                           source: transparentImage
-                        }               
-                     }
-                  });
-               }, 200);
-            }
-            break;
-
-         case 8:
-            if (this.state.ballon8.source === redBallonImage){
-               this.setState(prevState => {
-                  return {
-                     ...prevState,
-                     ballon8: {
-                        source: zvezdiceImage
-                     },
-                     ballonClickedCount: prevState.ballonClickedCount + 1            
-                  }
-               });
-               setTimeout(() => {
-                  this.setState(prevState => {
-                     return {
-                        ...prevState,
-                        ballon8: {
-                           source: transparentImage
-                        }               
-                     }
-                  });
-               }, 200);
-            }
-            break;
-
-         case 9:
-            if (this.state.ballon9.source === redBallonImage){
-               this.setState(prevState => {
-                  return {
-                     ...prevState,
-                     ballon9: {
-                        source: zvezdiceImage
-                     },
-                     ballonClickedCount: prevState.ballonClickedCount + 1           
-                  }
-               });
-               setTimeout(() => {
-                  this.setState(prevState => {
-                     return {
-                        ...prevState,
-                        ballon9: {
-                           source: transparentImage
-                        }               
-                     }
-                  });
-               }, 200);
-            }
-            break;
-
-         case 10:
-            if (this.state.ballon10.source === redBallonImage){
-               this.setState(prevState => {
-                  return {
-                     ...prevState,
-                     ballon10: {
-                        source: zvezdiceImage
-                     },
-                     ballonClickedCount: prevState.ballonClickedCount + 1           
-                  }
-               });
-               setTimeout(() => {
-                  this.setState(prevState => {
-                     return {
-                        ...prevState,
-                        ballon10: {
-                           source: transparentImage
-                        }               
-                     }
-                  });
-               }, 200);
-            }
-            break;
-
-         case 11:
-            if (this.state.ballon11.source === redBallonImage){
-               this.setState(prevState => {
-                  return {
-                     ...prevState,
-                     ballon11: {
-                        source: zvezdiceImage
-                     },
-                     ballonClickedCount: prevState.ballonClickedCount + 1           
-                  }
-               });
-               setTimeout(() => {
-                  this.setState(prevState => {
-                     return {
-                        ...prevState,
-                        ballon11: {
-                           source: transparentImage
-                        }               
-                     }
-                  });
-               }, 200);
-            }
-            break;
-
-         case 12:
-            if (this.state.ballon12.source === redBallonImage){
-               this.setState(prevState => {
-                  return {
-                     ...prevState,
-                     ballon12: {
-                        source: zvezdiceImage
-                     },
-                     ballonClickedCount: prevState.ballonClickedCount + 1           
-                  }
-               });
-               setTimeout(() => {
-                  this.setState(prevState => {
-                     return {
-                        ...prevState,
-                        ballon12: {
-                           source: transparentImage
-                        }               
-                     }
-                  });
-               }, 200);
-            }
-            break;
-      
-         default:
-            break;
-      }
-      
-      // setTimeout(() => {
-      //    alert(this.state.ballonClickedCount);
-      // }, 1);
-      
-      
+      this.setState(prevState => {
+         return {
+            ...prevState,
+            ballonClickedCount: prevState.ballonClickedCount + 1         
+         }      
+      });
    }
+
+
 
 
    render() {
@@ -395,30 +68,30 @@ class RazlikaZadatak1Screen1 extends Component {
                {flexDirection: 'column'} : 
                {flexDirection: 'row'}]}
             >
-               <Ballon ref={ref => this.lj = ref} source={this.state.ballon1.source} onPress={() => this.imageClick(1)}/>
-               <Ballon source={this.state.ballon2.source} onPress={() => this.imageClick(2)}/>
-               <Ballon source={this.state.ballon3.source} onPress={() => this.imageClick(3)}/>
-               <Ballon source={this.state.ballon4.source} onPress={() => this.imageClick(4)}/>
+               <Ballon onPress={() => this.imageClick(1)} ref={ref => this.lj = ref} />
+               <Ballon onPress={() => this.imageClick(2)}/>
+               <Ballon onPress={() => this.imageClick(3)}/>
+               <Ballon onPress={() => this.imageClick(4)}/>
             </View>
             <View style={[styles.ballonContainer,
                this.state.viewMode === 'portrait' ?
                {flexDirection: 'column'} : 
                {flexDirection: 'row'}]}
             >
-               <Ballon source={this.state.ballon5.source} onPress={() => this.imageClick(5)}/>
-               <Ballon source={this.state.ballon6.source} onPress={() => this.imageClick(6)}/>
-               <Ballon source={this.state.ballon7.source} onPress={() => this.imageClick(7)}/>
-               <Ballon source={this.state.ballon8.source} onPress={() => this.imageClick(8)}/>
+               <Ballon onPress={() => this.imageClick(5)}/>
+               <Ballon onPress={() => this.imageClick(6)}/>
+               <Ballon onPress={() => this.imageClick(7)}/>
+               <Ballon onPress={() => this.imageClick(8)}/>
             </View>
             <View style={[styles.ballonContainer,
                this.state.viewMode === 'portrait' ?
                {flexDirection: 'column'} : 
                {flexDirection: 'row'}]}
             >               
-               <Ballon source={this.state.ballon9.source} onPress={() => this.imageClick(9)}/>
-               <Ballon source={this.state.ballon10.source} onPress={() => this.imageClick(10)}/>
-               <Ballon source={this.state.ballon11.source} onPress={() => this.imageClick(11)}/>
-               <Ballon source={this.state.ballon12.source} onPress={() => this.imageClick(12)}/>
+               <Ballon onPress={() => this.imageClick(9)}/>
+               <Ballon onPress={() => this.imageClick(10)}/>
+               <Ballon onPress={() => this.imageClick(11)}/>
+               <Ballon onPress={() => this.imageClick(12)}/>
             </View>
             
          </View>
