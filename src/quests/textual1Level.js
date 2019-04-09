5e6f29a876a21aaa2e53869d0f02df8980d12ee5
Ljubixonovich <vikvikvuk@gmail.com>
2019-04-09 14:07:31 +0200

    added template literals

--- src/quests/textual1Level.js
+++ src/quests/textual1Level.js
@@ -14,14 +14,11 @@ export const addQuest1 = () => {
    const number1 = Math.floor(Math.random() * limit) + 1;
    const number2 = Math.floor(Math.random() * (limit + 1 - number1));
 
-   // const number1 = Math.floor(Math.random() * 12); 
-   // const number2 = Math.floor(Math.random() * (13 - number1));
-
-   let taskText = name1.nomin + ' ima ' + number1 + (number1 === 1 ? ' kliker' : ' klikera') + '. ' +
-      name2.nomin + ' ima ' + number2 + (number2 === 1 ? ' kliker' : ' klikera') +
-      '. Koliko ' + name1.nomin + ' i ' + name2.nomin + ' imaju zajedno klikera?';
+   let taskText = `${name1.nomin} ima ${number1} ${(number1 === 1 ? ' kliker' : ' klikera')}. ` +
+      `${name2.nomin } ima ${number2} ${(number2 === 1 ? ' kliker' : ' klikera')}. ` +
+      `Koliko ${name1.nomin} i ${name2.nomin} imaju zajedno klikera?`;
    let correctResult = number1 + number2;
-   let helpText = number1 + ' + ' + number2 + ' =';
+   let helpText = `${number1} + ${number2} =`;
 
 
    return {
@@ -42,13 +39,12 @@ export const substractQuest1 = () => {
    const number1 = Math.floor(Math.random() * limit) + 1;
    const number2 = Math.floor(Math.random() * number1);
 
-   let taskText = name1.nomin + ' je ' + (name1.male ? ' imao ' : ' imala ') + number1 + 
-      (number1 === 1 ? ' kliker' : ' klikera') +
-      ', pa je onda ' + (name1.male ? 'poklonio ' : 'poklonila ') + name2.dativ + ' ' + number2 + 
-      (number2 === 1 ? ' kliker' : ' klikera') +
-      '. Koliko je ' + name1.dativ + ' ostalo klikera ?';
+   let taskText = `${name1.nomin} je ${(name1.male ? ' imao ' : ' imala')}` + 
+      `${number1} ${(number1 === 1 ? ' kliker' : ' klikera')}, pa je onda ` + 
+      `${(name1.male ? 'poklonio ' : 'poklonila ')} ${name2.dativ} ${number2} ` +
+      `${(number2 === 1 ? ' kliker' : ' klikera')}. Koliko je ${name1.dativ} ostalo klikera ?`;
    let correctResult = number1 - number2;
-   let helpText = number1 + ' - ' + number2 + ' =';
+   let helpText = `${number1} - ${number2} =`;
 
 
    return {
